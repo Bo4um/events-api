03f56ed6a6cc4f1cfe8e3e580e15d1fe38f30d3c
Dmitry Belyatskiy <dmitry.belyatskiy@gmail.com>
2023-01-13 13:51:46 +0300

    Project created, hibernate configured

---
+++ src/main/java/com/example/events/model/Event.java
@@ -0,0 +1,78 @@
+package com.example.events.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "events_api")
+public class Event {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "theme")
+    private String theme;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "organizer")
+    private String organizer;
+
+    @Column(name = "datetime")
+    private Date datetime;
+
+    @Column(name = "location")
+    private String location;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
